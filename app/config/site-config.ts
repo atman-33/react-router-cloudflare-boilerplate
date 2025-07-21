5795f224b0d516c89070658bb0d8fb11a7ac1b91
atman-33 <gpbjk0304@gmail.com>
2025-07-21 22:52:18 +0900

    feat: add seo etc.

---
+++ app/config/site-config.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: "YourApp",
+  description: "Write your app description here.",
+  appUrl: "https://yourapp.com",
+  xUrl: "https://x.com/atman_33",
+  githubUrl: "https://github.com/atman-33",
+  keywords: [""],
+  author: "atman-33",
+  ogpImage: "/ogp-image.png",
+};
